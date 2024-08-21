33c15566344b3abb219b3e15525f5072cde05919
duncaiyun <759830766@qq.com>
2024-08-21 16:05:31 +0800

    月度账单-切换弹窗功能

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,7 +1,16 @@
 import { NavBar, DatePicker } from 'antd-mobile'
 import './index.scss'
+import { useState } from 'react'
+import classNames from 'classnames'
 
 const Month = () => {
+  // 控制弹窗的打开和关闭
+  const [dateVisible,setDateVisible] = useState(false)
+  const onConfirm = ()=>{
+    setDateVisible(false)
+    // 其他逻辑
+    
+  }
   return (
     <div className="monthlyBill">
       <NavBar className="nav" backArrow={false}>
@@ -10,11 +19,12 @@ const Month = () => {
       <div className="content">
         <div className="header">
           {/* 时间切换区域 */}
-          <div className="date">
+          <div className="date" onClick={()=>setDateVisible(true)}>
             <span className="text">
               2023 | 3月账单
             </span>
-            <span className='arrow expand'></span>
+            {/* 思路：根据当前弹窗打开的状态控制expend类名是否存在 */}
+            <span className={classNames('arrow',{expand:dateVisible})}></span>
           </div>
           {/* 统计区域 */}
           <div className='twoLineOverview'>
@@ -36,7 +46,10 @@ const Month = () => {
             className="kaDate"
             title="记账日期"
             precision="month"
-            visible={false}
+            visible={dateVisible}
+            onClose={()=>setDateVisible(false)}
+            onConfirm={onConfirm}
+            onCancel={()=>setDateVisible(false)}
             max={new Date()}
           />
         </div>
