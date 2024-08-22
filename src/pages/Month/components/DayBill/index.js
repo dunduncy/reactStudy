54db5e19232ddaef0607388d78c9d55c4aead035
duncaiyun <dun.caiyun@relectric.cn>
2024-08-22 19:23:43 +0800

    记账功能熟悉和模板搭建

--- src/pages/Month/components/DayBill/index.js
+++ src/pages/Month/components/DayBill/index.js
@@ -2,7 +2,7 @@ import classNames from 'classnames'
 import './index.scss'
 import { useMemo, useState } from 'react'
 import { billTypeToName } from '@/contants'
-import Icon from '@/components/icon'
+import Icon from '@/components/Icon'
 
 const DailyBill = ({ date, billList }) => {
     const dayResult = useMemo(() => {
