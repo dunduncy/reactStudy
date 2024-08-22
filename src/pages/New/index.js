4e2a726429271a8f18f2d569488999bf5303d4ae
duncaiyun <dun.caiyun@relectric.cn>
2024-08-22 20:15:42 +0800

    新增账单收尾工作

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import { addBillList } from '@/store/modules/billStore'
 import { useDispatch } from 'react-redux'
+import dayjs from 'dayjs'
 
 const New = () => {
   const navigate = useNavigate()
@@ -27,10 +28,19 @@ const New = () => {
     const data = {
         type:billType,
         money:billType==='pay'?-money:+money,
-        date:new Date(),
+        date,
         useFor
     }
     dispatch(addBillList(data))
+  }
+  // 存储选择的时间
+  const [date,setDate] = useState()
+  // 控制时间打开关闭
+  const [dateVisible, setDateVisible] = useState(false)
+  // 确认选择时间
+  const dateConfirm = (value) => {
+    setDate(value)
+    setDateVisible(false)
   }
   return (
     <div className="keepAccounts">
@@ -60,11 +70,14 @@ const New = () => {
           <div className="kaForm">
             <div className="date">
               <Icon type="calendar" className="icon" />
-              <span className="text">{'今天'}</span>
+              <span className="text" onClick={()=>setDateVisible(true)}>{dayjs(date).format('YYYY-MM-DD')}</span>
+              {/* 时间选择器 */}
               <DatePicker
                 className="kaDate"
                 title="记账日期"
                 max={new Date()}
+                visible={dateVisible}
+                onConfirm={dateConfirm}
               />
             </div>
             <div className="kaInput">
@@ -93,7 +106,7 @@ const New = () => {
                     <div
                       className={classNames(
                         'item',
-                        ''
+                         useFor===item.type?'selected':''
                       )}
                       key={item.type}
                       onClick={()=>setUseFor(item.type)}
