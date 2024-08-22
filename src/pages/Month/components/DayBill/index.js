3fddc3cb81988aef6be969f62bca5658581fc8c8
duncaiyun <759830766@qq.com>
2024-08-22 17:11:49 +0800

    账单图标组件的封装

--- src/pages/Month/components/DayBill/index.js
+++ src/pages/Month/components/DayBill/index.js
@@ -2,6 +2,7 @@ import classNames from 'classnames'
 import './index.scss'
 import { useMemo, useState } from 'react'
 import { billTypeToName } from '@/contants'
+import Icon from '@/components/icon'
 
 const DailyBill = ({ date, billList }) => {
     const dayResult = useMemo(() => {
@@ -43,6 +44,8 @@ const DailyBill = ({ date, billList }) => {
                 {billList.map(item => {
                     return (
                         <div className="bill" key={item.id}>
+                            {/* 图标 */}
+                            <Icon type={item.useFor}></Icon>
                             <div className="detail">
                                 <div className="billType">{billTypeToName[item.useFor]}</div>
                             </div>
